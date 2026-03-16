d07557b57d2c1b853083a92807996ee85ab91928
nooanooa <nooa.jarvinen@edu.salpaus.fi>
2026-03-16 13:29:46 +0200

    Updated files

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const request = new Request("https://api.kirjastot.fi/v4/library");
-
 const button = document.getElementById("search");
 
 button.addEventListener("click",SearchLibrary);
 
-function SearchLibrary(){
-    window.fetch(request)
+async function SearchLibrary(){
+    const value = document.getElementById("librarysearch").value.toLowerCase();
+    await fetch(`https://api.kirjastot.fi/v4/library?name=${value}`)
         .then((res) => {
 
         if (!res.ok) {
@@ -15,20 +14,38 @@ function SearchLibrary(){
         return res.json();
         })
         .then((data) => {
-        // const value = document.getElementById("librarysearch").value.toLowerCase();
 
         for (const k of Object.entries(data.items)) {
+            const v = k[1];
             const template = document.createElement("div");
             const name = document.createElement("p");
             const img = document.createElement("img");
             const location = document.createElement("p");
+            const slog = document.createElement("p");
+            const mapslocation = document.createElement("iframe");
 
-            name.innerHTML = k[1].name;
-            img.src = k[1].coverPhoto.small.url;
-            location.innerHTML = ` ${k[1].address.street}, ${k[1].address.city}`;
+
+            img.style.width = "100%"
+
+            mapslocation.src = `https://www.google.com/maps?q=${v.coordinates.lat},${v.coordinates.lon}&z=15&output=embed`
+            mapslocation.width = "100%"
+            mapslocation.height = "300px"
+            mapslocation.style.border = "0"
+            mapslocation.loading = "lazy"
+
+            name.textContent = v.name;
+            slog.textContent = v.slogan;
+            location.innerHTML = ` ${v.address.street}, ${v.address.city}`;
+            if (v.coverPhoto != null) {
+                img.src = v.coverPhoto.medium.url;
+            }else {continue}
+
+
             template.appendChild(name);
             template.appendChild(img);
+            template.appendChild(slog);
             template.appendChild(location);
+            template.appendChild(mapslocation);
             document.getElementById("library").appendChild(template);
         };
 
